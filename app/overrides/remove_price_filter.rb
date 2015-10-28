6a5cd4080131430b539431ab61d393bbc0a41398
Mariano Matayoshi <matayoshi.mariano@gmail.com>
2015-10-28 11:39:02 -0300

    Update remove_price_filter.rb

--- app/overrides/remove_price_filter.rb
+++ app/overrides/remove_price_filter.rb
@@ -1,6 +1,1 @@
-Deface::Override.new(
-  :virtual_path => "spree/shared/_filters",
-  :name         => "remove_price_filter",
-  :replace      => 'erb[silent]:contains("unless filters.empty?")',
-  :text         => "<% if !filters.empty? && Spree::Cart.first.cart_status %>"
-)
+
